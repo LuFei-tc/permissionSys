c952f80a9d96834e63b3921debf6f86f3627b375
sxhJoker <sxhjoker@163.com>
2019-04-15 21:46:20 +0800

    配置文件修改

--- src/main/java/com/ycsf/persys/PermissionSysApplication.java
+++ src/main/java/com/ycsf/persys/PermissionSysApplication.java
@@ -5,16 +5,17 @@ import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreato
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.ycsf.persys.modalur.sys.dao")
 
-public class PermissionSysApplication {
+public class PermissionSysApplication{
 
-	
-	@Bean
+    @Bean
     @ConditionalOnMissingBean
     public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
         DefaultAdvisorAutoProxyCreator defaultAAP = new DefaultAdvisorAutoProxyCreator();
